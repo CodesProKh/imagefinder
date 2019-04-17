140c7892e67d7ce6c491e25fd8d4636ac0eef098
Tykun <tykun.khy@hotmail.com>
2019-04-17 16:32:43 -0500

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <AppBar title="Pixabay Image Finder" />
+    </React.Fragment>
+  );
+};
+export default Navbar;
